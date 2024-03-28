4f2d3055196535be7e4c63dd862c1ccd2220c88d
gautam-u-hub <72206790+gautam-u-hub@users.noreply.github.com>
2024-03-28 11:25:34 +0530

    Update policyActions.js

--- src/store/policyActions.js
+++ src/store/policyActions.js
@@ -5,7 +5,7 @@ export const getAllPolicies = () => {
     return async (dispatch) => {
         try {
 
-            let link = `http://localhost:4000/policies`;
+            let link = `https://claims-management-system.onrender.com/policies`;
 
         
             const { data } = await axios.get(link);
